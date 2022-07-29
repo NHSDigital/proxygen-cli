35c54cfe59f7e95e007b19fea3ad4378dd304fb7
dominicplatt2-nhs <91538443+dominicplatt2-nhs@users.noreply.github.com>
2022-07-29 12:08:46 +0100

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
+echo "Uninstalling proxygen"
 rm -rf ~/.paas
 rm -f ~/.paasrc
 sed -i '/.paas\/bin:$PATH/d' ~/.bashrc
 sed -i '/.paas\/bin:$PATH/d' ~/.profile
+echo "Proxygen is now uninstalled!"
