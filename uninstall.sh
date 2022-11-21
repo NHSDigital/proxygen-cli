87640b65f7a57bd853cee5e876222148ec413fbc
Ben Strutt <ben.strutt1@nhs.net>
2022-11-21 09:21:33 +0000

    Nuke cli v1

--- uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Uninstalling proxygen"
-rm -rf ~/.paas
-rm -f ~/.paasrc
-sed -i '/.paas\/bin:$PATH/d' ~/.bashrc
-sed -i '/.paas\/bin:$PATH/d' ~/.profile
-echo "Proxygen is now uninstalled!"
