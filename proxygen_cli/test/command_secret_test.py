7aa98e543ab4df956f725b998b550907ab1b4137
Jon Marston <john.marston1@nhs.net>
2023-08-04 12:47:31 +0100

    APM-4575. Started to added tests for secrets.

---
+++ proxygen_cli/test/command_secret_test.py
@@ -0,0 +1,117 @@
+import os
+from urllib.parse import urlparse
+
+from click.testing import CliRunner
+
+import proxygen_cli.cli.command_secret as cmd_secret
+
+API_NAME = "mock-api"
+API_ENV = "internal-dev"
+SECRET_NAME = "test-secret"
+
+
+def _parse_fixture(filename):
+
+    test_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = f"{test_dir}/fixtures/{filename}"
+
+    with open(file_path, "r", encoding="utf-8") as f_desc:
+        return file_path, f_desc.read()
+
+
+def _url_path(url) -> str:
+    """
+    Strip the path from a URL.
+    :param url: The URL to strip.
+    :return: The URL without the path.
+    """
+    parsed = urlparse(url)
+    return parsed.path
+
+
+def test_secret_put(patch_access_token, patch_request):
+    """
+    Ensure that the PUT reqeust for secrets contains the expected fields
+    and is to the correct URL.
+    """
+    runner = CliRunner()
+
+    file_path, file_contents = _parse_fixture("valid_pkcs12.pks")
+
+    with patch_access_token(), patch_request(200, {}) as patched_request:
+        runner.invoke(
+            cmd_secret.put,
+            [
+                API_ENV,
+                "test-secret",
+                "--secret-file",
+                file_path
+            ],
+            obj={"api": API_NAME},
+        )
+
+        _, (verb, url), content = patched_request.mock_calls[0]
+        assert verb == "PUT"
+        assert _url_path(url) \
+            == f"/apis/{API_NAME}/environments/{API_ENV}/secrets/{SECRET_NAME}"
+        assert file_contents == content['data']
+        assert content['params'] == {}
+
+
+def test_secret_put_apikey(patch_access_token, patch_request):
+    """
+    Ensure that the PUT request for an apikey secret contains the expected
+    fields and is to the correct URL.
+    """
+    runner = CliRunner()
+    secret_value = "TESTSECRET"
+
+    with patch_access_token(), patch_request(200, {}) as patched_request:
+        runner.invoke(
+            cmd_secret.put,
+            [
+                API_ENV,
+                "test-secret",
+                "--apikey",
+                "--secret-value",
+                secret_value
+            ],
+            obj={"api": API_NAME},
+        )
+
+        _, (verb, url), content = patched_request.mock_calls[0]
+        assert verb == "PUT"
+        assert _url_path(url) \
+            == f"/apis/{API_NAME}/environments/{API_ENV}/secrets/{SECRET_NAME}"
+        assert content['params']['type'] == 'apikey'
+        assert content['data'] == secret_value
+
+
+def test_secret_put_mtls(patch_access_token, patch_request):
+    """
+    Ensure that the PUT request for an mtls secret contains the expected
+    fields and is to the correct URL.
+    The Proxygen server contains PKCS12 validation logic.
+    """
+    runner = CliRunner()
+    secret_value = "TESTSECRET"
+
+    with patch_access_token(), patch_request(200, {}) as patched_request:
+        runner.invoke(
+            cmd_secret.put,
+            [
+                API_ENV,
+                "test-secret",
+                "--apikey",
+                "--secret-value",
+                secret_value
+            ],
+            obj={"api": API_NAME},
+        )
+
+        _, (verb, url), content = patched_request.mock_calls[0]
+        assert verb == "PUT"
+        assert _url_path(url) \
+            == f"/apis/{API_NAME}/environments/{API_ENV}/secrets/{SECRET_NAME}"
+        assert content['params']['type'] == 'mtls'
+        assert content['data'] == secret_value
