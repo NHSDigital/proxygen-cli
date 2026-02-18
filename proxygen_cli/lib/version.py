f437cf6704f4923bd80c77ac69cc48efb35c5861
Sophie Somerville <sophie.somerville@nhs.net>
2026-02-18 15:00:35 +0000

    Remove unneeded packaging dependency

--- proxygen_cli/lib/version.py
+++ proxygen_cli/lib/version.py
@@ -1,12 +1,9 @@
-from packaging.version import parse
-
 from proxygen_cli.lib import proxygen_api
 from proxygen_cli import __version__ as proxygen_cli_version
 
-
 def validate_cli_version():
     status = proxygen_api.status()
-    required_cli_version = parse(status["proxygen_cli"]["min_version"])
-    current_cli_version = parse(proxygen_cli_version)
-    if current_cli_version < required_cli_version:
-        raise RuntimeError(f"This version proxygen-cli is out-of-date. Please update to {required_cli_version}")
+    required = tuple(int(x) for x in status["proxygen_cli"]["min_version"].split("."))
+    current = tuple(int(x) for x in proxygen_cli_version.split("."))
+    if current < required:
+        raise RuntimeError(f"This version proxygen-cli is out-of-date. Please update to {status['proxygen_cli']['min_version']}")
